7e57d98477e5f35d9e698ac95f8fc4bb7648f10f
Joshua Tagoe <jnt297@nyu.edu>
2019-04-12 16:41:58 -0400

    Part 4 Configuration File Complete

--- src/app.js
+++ src/app.js
@@ -6,3 +6,10 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, '/public')));
 app.set('view engine', 'hbs');
 app.use(express.urlencoded({ extended: false }));
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000);
--- src/db.js
+++ src/db.js
@@ -1,4 +1,24 @@
 // db.js
+
+// is the environment variable, NODE_ENV, set to PRODUCTION? 
+let dbconf;
+if (process.env.NODE_ENV === 'PRODUCTION') {
+ // if we're in PRODUCTION mode, then read the configration from a file
+ // use blocking file io to do this...
+ const fs = require('fs');
+ const path = require('path');
+ const fn = path.join(__dirname, 'config.json');
+ const data = fs.readFileSync(fn);
+
+ // our configuration file will be in json, so parse it and set the
+ // conenction string appropriately!
+ const conf = JSON.parse(data);
+ dbconf = conf.dbconf;
+} else {
+ // if we're not in PRODUCTION mode, then use
+ dbconf = 'mongodb://Timbuktu';
+}
+
 const mongoose = require('mongoose') ;
 URLSlugs = require('mongoose-url-slugs');
 //mongoose.connect('mongodb://localhost/hw05');
@@ -35,5 +55,4 @@ const productSchema = new mongoose.Schema({
 
 //mongoose.connect('mongodb://localhost/hw05');
 
-
-
+mongoose.connect(dbconf);
