f1161cc061baae0f983f56ed0bec9556f5aeb389
Joshua Tagoe <34180299+JUCHY@users.noreply.github.com>
2019-06-27 21:06:12 -0400

    FooterEdits

--- components/FooterInfo.jsx
+++ components/FooterInfo.jsx
@@ -3,14 +3,79 @@ export default class FooterInfo extends React.Component{
     render(){
         return(
             <div className='footer'>
+
+                <ul>
+                    <li className="title">Title</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>                    
+                </ul>
+                <ul>
+                    <li className="title">Title</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>                    
+                </ul>
+                <ul>
+                    <li className="title">Title</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>                    
+                </ul>
+                <ul>
+                    <li className="title">Title</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>                    
+                </ul>
+                <ul>
+                    <li className="title">Title</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>
+                    <li>Link#1</li>                    
+                </ul>
+                
+                
                 <style jsx>{`
                     
                     .footer{
                         background-color:#707070;
                         height: 60vh;
+                        display: flex;
+                        justify-content: space-evenly;
 
                     }
-                    
+                    ul{
+                        list-style: none;
+                        
+                    }
+                    .title{
+                        font-size:2rem;
+                        font-weight: bolder;
+                    }
+                    li{
+                        padding: 2vh;
+                    }
                     
                     `}</style>
             </div>
